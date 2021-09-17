f815c2cc0c0c1ff93a0bbba6a432af6ce2929148
Ben Kinard <benjaminjameskinard@gmail.com>
2021-09-17 17:06:34 -0600

    Implement partitioning priorities 1-3

--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -46,18 +46,22 @@ public class EvilHangmanGame implements IEvilHangmanGame {
             guessedLetters.add(guess);
         }
 
-        // Create a Map to store partitions
+        // Create a Map to store partitions and variables to track the largest set and minimum occurrences of guess
         Map<String, Set<String>> partitions = new HashMap<>();
+        int maxSize = 0;
+        int minOccurrences = 100;
 
         // Loop through the words in the dictionary and create the partitions
         for(String word : dictionary) {
+            int curOccurrences = 0;
             // Create StringBuilder variable to store partition pattern
             StringBuilder pattern = new StringBuilder();
             // Loop through each character of current word
             for(int i = 0; i < word.length(); i++) {
-                // If current character matches the guess, append guess to the StringBuilder
+                // If current character matches the guess, append guess to the StringBuilder and increment occurrences
                 if(word.charAt(i) == guess) {
                     pattern.append(guess);
+                    curOccurrences++;
                 } else { // Otherwise, append an underscore to the StringBuilder
                     pattern.append('_');
                 }
@@ -70,8 +74,47 @@ public class EvilHangmanGame implements IEvilHangmanGame {
             } else {
                 partitions.get(key).add(word);
             }
+            // Update maxSize and minOccurrences if necessary
+            if(partitions.get(key).size() > maxSize) {
+                maxSize = partitions.get(key).size();
+            }
+            if(curOccurrences < minOccurrences) {
+                minOccurrences = curOccurrences;
+            }
         }
+        // Keep only the largest partition(s)
+        for(Iterator<Map.Entry<String, Set<String>>> itr = partitions.entrySet().iterator(); itr.hasNext();) {
+            if(itr.next().getValue().size() < maxSize) {
+                itr.remove();
+            }
+        }
+        // If there are multiple partitions of the largest size, pare it down to one partition
+        if(partitions.size() > 1) {
+            // If there is a partition that does not include the guessed letter, choose that one
+            for(String pattern : partitions.keySet()) {
+                if(!pattern.contains(Character.toString(guess))) {
+                    return partitions.get(pattern);
+                }
+            }
+            // If each partition has the guessed letter, choose the one with the fewest occurrences of the letter
+            for(Iterator<Map.Entry<String, Set<String>>> itr = partitions.entrySet().iterator(); itr.hasNext();) {
+                int curOccurrences = 0;
+                String pattern = itr.next().getKey();
+                for(int i = 0; i < pattern.length(); i++) {
+                    if(pattern.charAt(i) == guess) {
+                        curOccurrences++;
+                    }
+                }
+                if(curOccurrences > minOccurrences) {
+                    itr.remove();
+                }
+            }
+            // If there are multiple partitions with the minimum # of occurrences, pare it down to one partition
+            if(partitions.size() > 1) {
 
+            }
+
+        }
 
     }
 
