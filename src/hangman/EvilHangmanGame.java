bf36e24ec94bd4d2159854b04878db156127399b
Ben Kinard <benjaminjameskinard@gmail.com>
2021-09-17 14:02:39 -0600

    Implement EvilHangmanGame startGame method

--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -2,6 +2,8 @@ package hangman;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.SortedSet;
 
@@ -15,7 +17,19 @@ public class EvilHangmanGame implements IEvilHangmanGame {
 
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
-
+        // Clear any previous dictionary/set of words
+        if(this.dictionary != null) {
+            this.dictionary.clear();
+        }
+        // Initialize EvilHangmanGame's dictionary to be an empty set
+        this.dictionary = new HashSet<>();
+        // Read the words from the dictionary file into the EvilHangmanGame's dictionary (set of words)
+        Scanner scanner = new Scanner(dictionary);
+        while(scanner.hasNext()) {
+            this.dictionary.add(scanner.next());
+        }
+        // Remove any words from EvilHangmanGame's dictionary that are longer than wordLength
+        this.dictionary.removeIf(s -> s.length() > wordLength);
     }
 
     @Override
